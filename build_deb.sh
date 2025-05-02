71cc85a2f1d50faaab7c4214573301ec897bb616
oleg <olegpustovalov220@gmail.com>
2025-05-02 22:06:22 +0300

    ver 1.0.2

--- build_deb.sh
+++ build_deb.sh
@@ -1,24 +1,19 @@
 #!/bin/bash
 
-# Переменные
 APP_NAME="wor-sys"
 VERSION="1.0"
 BUILD_DIR="deb_build"
 INSTALL_DIR="${BUILD_DIR}/${APP_NAME}/usr/local/bin/${APP_NAME}"
-ICON_DIR="${BUILD_DIR}/${APP_NAME}/usr/share/icons/hicolor/256x256/apps"
-DESKTOP_DIR="${BUILD_DIR}/${APP_NAME}/usr/share/applications"
 
-# Очистка старых сборок
+# Очистка
 rm -rf "$BUILD_DIR"
 mkdir -p "$INSTALL_DIR"
-mkdir -p "$ICON_DIR"
-mkdir -p "$DESKTOP_DIR"
 
 # Копируем файлы
 cp app.py logo.png requirements.txt "$INSTALL_DIR"
 chmod +x "$INSTALL_DIR/app.py"
 
-# Создаем DEBIAN/control
+# DEBIAN/control
 mkdir -p "${BUILD_DIR}/${APP_NAME}/DEBIAN"
 cat > "${BUILD_DIR}/${APP_NAME}/DEBIAN/control" <<EOF
 Package: ${APP_NAME}
@@ -28,26 +23,19 @@ Priority: optional
 Architecture: all
 Depends: python3, python3-gi, gir1.2-gtk-3.0, gir1.2-appindicator3-0.1, python3-psutil
 Maintainer: You <olegpustovalov220@gmail.com>
-Description: System tray monitor.
+Description: Power tray monitor.
 EOF
 
-# Создаем .desktop файл
-cat > "${DESKTOP_DIR}/${APP_NAME}.desktop" <<EOF
+# .desktop файл
+mkdir -p "${BUILD_DIR}/${APP_NAME}/usr/share/applications"
+cat > "${BUILD_DIR}/${APP_NAME}/usr/share/applications/${APP_NAME}.desktop" <<EOF
 [Desktop Entry]
 Name=SysMon
 Exec=python3 /usr/local/bin/${APP_NAME}/app.py
-Icon=${APP_NAME}
+Icon=/usr/local/bin/${APP_NAME}/logo.png
 Type=Application
 Categories=Utility;
 EOF
 
-# Копируем иконку
-cp logo.png "${ICON_DIR}/${APP_NAME}.png"
-
-# Создание .deb пакета
+# Создание .deb
 dpkg-deb --build "${BUILD_DIR}/${APP_NAME}"
-
-# Удаление временных директорий после сборки
-rm -rf "$BUILD_DIR"
-
-echo "Deb package ${APP_NAME}-${VERSION}.deb created successfully!"
